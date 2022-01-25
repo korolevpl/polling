49479771f5fb491e225f0e679031ffb4bdd36d2f
korolevpl <korolev.home@gmail.com>
2022-01-25 20:29:06 +0300

    rename

--- src/main/java/com/polling/PollingApplication.java
+++ src/main/java/com/polling/PollingApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuickPollApplication {
+public class PollingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuickPollApplication.class, args);
+		SpringApplication.run(PollingApplication.class, args);
 	}
 
 }
